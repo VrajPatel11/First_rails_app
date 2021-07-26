a4ae09d288ed6e5deb3ca2eeef1c51b604db47ab
VrajPatel11 <patelvraj1132000@gmail.com>
2021-07-26 18:08:43 +0530

    Operations performed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account information successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Hey, #{@user.username} Welcome to Blog App"
+      redirect_to articles_path
+    else
+        render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
